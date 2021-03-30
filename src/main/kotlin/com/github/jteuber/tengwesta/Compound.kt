922cddf490a0a2d28f4f4a476c261f43e2e9e366
jteuber <jakob.teuber@gmx.de>
2021-03-30 16:58:46 +0200

    introduced InflectionClasses

--- src/main/kotlin/com/github/jteuber/tengwesta/Compound.kt
+++ src/main/kotlin/com/github/jteuber/tengwesta/Compound.kt
@@ -1,34 +1,16 @@
 package com.github.jteuber.tengwesta
 
-fun compound(prefix: Morpheme, suffix: Morpheme): String = when {
-    prefix.endsWith(consonants) && suffix.startsWith(consonants)
-    -> compoundConsonantAssimilation(prefix, suffix)
-    prefix.endsWith(vowels) && suffix.startsWith(vowels)
-    -> compoundVowelAssimilation(prefix, suffix)
-    else -> compoundLengthening(prefix, suffix)
-}
-
-fun compoundVowelAssimilation(prefix: Morpheme, suffix: Morpheme): String {
-    // TODO research
-    return prefix.regularized + suffix.regularized
-}
-
-fun compoundConsonantAssimilation(prefix: Morpheme, suffix: Morpheme): String {
-    return prefix.regularized.substring(0, prefix.regularized.length - 1) +
-            (consonantAssimilation[prefix.regularized.last().toString() + suffix.regularized.first()]
-                ?: prefix.regularized.last().toString() + suffix.regularized.first()) +
-            suffix.regularized.substring(1)
-}
-
-fun compoundLengthening(prefix: Morpheme, suffix: Morpheme): String {
+fun compoundWithLengthening(prefix: Morpheme, suffix: Morpheme): Morpheme {
     val noLengthening = suffix.heavySyllables.any { it }
             || suffix.syllableCount < 1
             || prefix.syllableCount < 3
             || prefix.heavySyllables.secondToLast()
             || prefix.heavySyllables.last()
-    return if (noLengthening) {
-        prefix.regularized + suffix.regularized
-    } else {
-        prefix.lengthenedLast + suffix.regularized
-    }
+    return Morpheme(
+        if (noLengthening) {
+            prefix.regularized + suffix.regularized
+        } else {
+            prefix.lengthenedLast + suffix.regularized
+        }
+    )
 }
--- src/main/kotlin/com/github/jteuber/tengwesta/InflectionClass.kt
+++ src/main/kotlin/com/github/jteuber/tengwesta/InflectionClass.kt
@@ -1,27 +1,72 @@
 package com.github.jteuber.tengwesta
 
-abstract class Class {
-    abstract val inflections: List<Inflection>
-}
-
 abstract class Inflection {
     abstract val name: String
-    abstract fun inflect(morpheme: Morpheme): Morpheme
-    abstract fun unInflect(morpheme: Morpheme): Morpheme
+    abstract fun inflect(word: Word): Word
+    abstract fun unInflect(morpheme: Morpheme): Word
 }
 
-class BasicInflection(
+class SimpleForm(
     override val name: String,
     val ending: String,
-    val newClasses: List<Class> = emptyList()
-): Inflection() {
-
-    override fun inflect(morpheme: Morpheme) = Morpheme(
-        compound(morpheme, Morpheme(ending)),
-        newClasses
+    val newInflections: List<Inflection> = emptyList()
+) : Inflection() {
+    override fun inflect(word: Word) = Word(
+        Morpheme(word.lexicalForm.regularized + ending),
+        newInflections
     )
 
-    override fun unInflect(morpheme: Morpheme): Morpheme {
+    override fun unInflect(morpheme: Morpheme): Word {
+        TODO("Not yet implemented")
+    }
+
+}
+
+class SimpleStem(
+    override val name: String,
+    val ending: String,
+    val newInflections: List<Inflection> = emptyList()
+) : Inflection() {
+    override fun inflect(word: Word) = Word(
+        Morpheme(word.stem.regularized + ending),
+        newInflections
+    )
+
+    override fun unInflect(morpheme: Morpheme): Word {
+        TODO("Not yet implemented")
+    }
+
+}
+
+class FormWithProsodicLengthening(
+    override val name: String,
+    val ending: String,
+    val newInflections: List<Inflection> = emptyList()
+): Inflection() {
+
+    override fun inflect(word: Word) = Word(
+        compoundWithLengthening(word.lexicalForm, Morpheme(ending)),
+        newInflections
+    )
+
+    override fun unInflect(morpheme: Morpheme): Word {
+        TODO("Not yet implemented")
+    }
+
+}
+
+class StemWithProsodicLengthening(
+    override val name: String,
+    val ending: String,
+    val newInflections: List<Inflection> = emptyList()
+): Inflection() {
+
+    override fun inflect(word: Word) = Word(
+        compoundWithLengthening(word.stem, Morpheme(ending)),
+        newInflections
+    )
+
+    override fun unInflect(morpheme: Morpheme): Word {
         TODO("Not yet implemented")
     }
 
