ae746bcfadb6eea6c6092624655e869d99f44f65
jteuber <jakob.teuber@gmx.de>
2021-03-31 15:59:00 +0200

    renamed class

--- src/test/kotlin/com/github/jteuber/tengwesta/CompoundTest.kt
+++ src/test/kotlin/com/github/jteuber/tengwesta/CompoundTest.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 
-internal class CompoundWithLengtheningTest {
+internal class CompoundTest {
 
     @Test
     fun test_compoundWithLengthening() {
