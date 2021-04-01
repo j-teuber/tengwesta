ce114975b88f79496991aead487b71338699bfe2
jteuber <jakob.teuber@gmx.de>
2021-04-01 11:59:55 +0200

    intensive for adjectives

--- src/main/kotlin/com/github/jteuber/tengwesta/Adjectives.kt
+++ src/main/kotlin/com/github/jteuber/tengwesta/Adjectives.kt
@@ -1,25 +1,29 @@
 package com.github.jteuber.tengwesta
 
+val intensive = listOf(
+    FormWithAssimilationPrefix("intensive I (an-)", "an"),
+)
+
 val aAdjectives: List<Inflection> = listOf(
-    FormWithRemoval("plural (a)", "a", "e"),
+    FormWithRemoval("plural (a)", "a", "e", intensive),
     SimpleForm("adjective as noun (a)", "", aNoun),
-    FormWithProsodicLengthening("adverb (a-ea)", "ve")
+    FormWithProsodicLengthening("adverb (a-ea)", "ve", intensive)
 )
 
 val eaAdjectives = listOf(
-    FormWithRemoval("plural (ie)", "ea", "ie"),
+    FormWithRemoval("plural (ie)", "ea", "ie", intensive),
     SimpleForm("adjective as noun (a)", "", vocalicNoun),
-    FormWithProsodicLengthening("adverb (a-ea)", "ve")
+    FormWithProsodicLengthening("adverb (a-ea)", "ve", intensive)
 )
 
 val eAdjectives = listOf(
-    FormWithRemoval("plural (e)", "e", "i"),
+    FormWithRemoval("plural (e)", "e", "i", intensive),
     SimpleForm("adjective as noun (e)", "", eNoun),
-    FormWithRemoval("adverb (e)", "e", "ive")
+    FormWithRemoval("adverb (e)", "e", "ive", intensive)
 )
 
 val nAdjectives = listOf(
-    FormWithProsodicLengthening("plural (n)", "i"),
+    FormWithProsodicLengthening("plural (n)", "i", intensive),
     SimpleForm("adjective as noun (n)", "", nNouns),
-    SimpleForm("adverb (n)", "we")
+    SimpleForm("adverb (n)", "we", intensive)
 )
